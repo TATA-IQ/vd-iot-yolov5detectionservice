723034a5c2e42ea12af691ea39176c9bfbeb09b7
aditya.singh <aditya.singh@tataiq.com>
2023-10-20 11:52:13 +0000

    Yolov5Detection

--- yolov5/app/querymodel/imageModel.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Union
-class Image_Model(BaseModel):
-    image_name: Union[str, None] = None
-    image: Union[str, None] = None
-    model_config:Union[dict[str,str], None] = None
